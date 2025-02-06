0c3a9b9326e3fd64aa23ad8a798271487429b4a4
yantomchik <70806762+B4TARA@users.noreply.github.com>
2025-02-06 10:18:15 +0300

    test commit

--- KOP/KOP.BLL/obj/Debug/net6.0/KOP.BLL.AssemblyInfo.cs
+++ KOP/KOP.BLL/obj/Debug/net6.0/KOP.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KOP.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f5c68aaeef6dc54a86cecbbc4bd4a62b48d6dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673dd8f305498ce99d9c58c4b6a88aa7310b9047")]
 [assembly: System.Reflection.AssemblyProductAttribute("KOP.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KOP.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
