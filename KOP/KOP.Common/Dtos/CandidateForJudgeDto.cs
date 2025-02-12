6461e98be18702dfe73c4c00c906ffcb9df5d305
B4TARA <evgeniybaturel@gmail.com>
2025-02-12 17:07:30 +0300

    12/02/2025

---
+++ KOP/KOP.Common/Dtos/CandidateForJudgeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KOP.Common.Dtos
+{
+    public class CandidateForJudgeDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public bool HasJudged { get; set; }
+    }
+}
